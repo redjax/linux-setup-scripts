f1c89e34af3659a82bbea667afb64aaae2540946
Jack <jackenyon@gmail.com>
2019-09-26 13:46:36 -0400

    Code to get release information:
    Wrote code in test_release.py to pipe commands through the shell (namely 'lsb_release -a'), and return the output to a variable.
    
    I wanted to be able to run the lsb_release command and extract information such as Linux distro, release number, etc. from the output, but I was having difficulty working with the output directly. Instead, the program now writes the output to a text file, and I have a function that runs through the lines of the output and extracts what I'm looking for, which works well.
    
    Next I intend to create a JSON file populated with release information, and use that to build a class "this_pc," which will have instructions on commands to use for the current PC and its OS.
    
    This has only been tested on a single Ubuntu computer.

--- test_release.py
+++ test_release.py
@@ -1,21 +1,77 @@
 import subprocess
 
-
-def build_cmd(cmd):
-    """Takes a command (cmd) passed as a list of parameters
-    and returns the result."""
-    result = subprocess.Popen(cmd,
-                              stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE)
-
-    output, err = result.communicate(b"input data that is passed to subprocess' stdin")
-    rc = result.returncode
-
-    return rc
-
-
+# Shell command to get release information
 release_cmd = ['lsb_release', '-a']
-test_command = ['ls', '-a', '/home/jack/Documents/git/linux-setup-scripts']
+test_cmd = ['ls', '-a', '/home/jack/']
 
-print(build_cmd(release_cmd))
+# Open shell and run command, pipe output to stdout var
+process = subprocess.Popen(release_cmd, stdout=subprocess.PIPE, shell=False)
+# Create decoded output from stdout var
+output = process.stdout.read().decode()
+
+# Write output to text file for reading later
+outfile_name = 'this_pc/testout.txt'
+
+outfile = open(outfile_name, 'w+')
+outfile.write(output)
+outfile.close()
+
+
+def find_distro_info(file, search_term):
+    """Find related information from text file.
+    TODO: Break out each search item into new function
+    for easier development/variable creation."""
+    # Open file passed as arg for reading
+    searchfile = open(file, 'r')
+    # Loop through file to find search_term
+    for line in searchfile:
+        if search_term in line:
+            return line
+    searchfile.close()
+
+
+def build_pc_info_file(file, data):
+    """Build a json file with data found about current PC."""
+    data = {
+        'os': "",
+        'distro': "",
+        'release': "",
+        'codename': ""
+    }
+
+    outfile = open(file, 'w+')
+    outfile.write(output)
+    outfile.close()
+
+
+# Find the Distributor ID, i.e. Ubuntu
+# find_distro_info(outfile_name, 'Distributor ID:')
+# Find the release version, i.e. 19.04
+# find_distro_info(outfile_name, 'Release:')
+# Find codename, i.e. disco
+
+def test_releaseinfo():
+    """Get text from release info file, strip down to essential info."""
+    # List of words to scan string for, then remove
+    remove_words_list = ['Distributor', 'ID:', ' ', 'Release:', 'Codename:', '\t']
+
+    # Create distributor object
+    distributor = find_distro_info('this_pc/testout.txt', 'Distributor ID:')
+    # Create release object
+    release = find_distro_info('this_pc/testout.txt', 'Release:')
+    # Create codename object
+    codename = find_distro_info('this_pc/testout.txt', 'Codename:')
+    # Next 3 lines create objects to be stripped of remove_words_list items
+    distributor_cleaned = distributor.split()
+    release_cleaned = release.split()
+    codename_cleaned = codename.split()
+
+    # Strip words from remove_words_list out of input text
+    distributor_result = [word for word in distributor_cleaned if word not in remove_words_list]
+    release_result = [word for word in release_cleaned if word not in remove_words_list]
+    codename_result = [word for word in codename_cleaned if word not in remove_words_list]
+
+    return distributor_result[0], release_result[0], codename_result[0]
+
+
+print(test_releaseinfo())
